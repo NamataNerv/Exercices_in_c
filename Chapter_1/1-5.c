1b0209cb122f2a5145265d24487307137cfc9d34
NamataNerv <namatanerv@gmail.com>
2024-09-10 22:27:47 -0300

    para manyana

--- Chapter_1/1-5.c
+++ Chapter_1/1-5.c
@@ -1,27 +1,15 @@
-/*
-Exercise 1-5. the temperature conversion program to print the table in
-reverse order, that is, from 300 degrees to O. []
-*/
+//leer el codigo manyana
 
 #include<stdio.h>
 
 int main (){
 
-    float Faren,cels;
-    float lower, upper, step;
-    lower=0;
-    upper=300;
-    step=20;
-
-    printf("Farenht | Celcius\n");
- 
-    Faren=upper;
-    while(Faren>=lower){
-        //Faren=((cels*9.0)/5.0)+32;
-        cels=(5.0/9.0)*(Faren-32.0);
-        printf("%7.0f\t| %6.1f  \n",Faren,cels);
-        Faren -= step;
+    if((getchar()!=EOF)==0){
+        printf("\nuno\n");    
     }
 
+    printf("xddd\n%d",(getchar()!=EOF));
+ 
+    
     return 0;
 }
